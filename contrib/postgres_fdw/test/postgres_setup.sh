a63f52b9bf4b39ab6f684e4694389709aa6129be
Xiaoran Wang <xiwang@pivotal.io>
2020-08-28 19:14:39 +0800

    run postgres_fdw test

---
+++ contrib/postgres_fdw/test/postgres_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eox pipefail
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd ${DIR}
+GPPORT=${PGPORT}
+export PGPORT=${PG_PORT}
+pgbin="pgsql"
+
+# install postgres
+if [ ! -d "${pgbin}" ] ; then
+	wget https://get.enterprisedb.com/postgresql/postgresql-10.14-1-linux-x64-binaries.tar.gz
+	tar xf postgresql-10.14-1-linux-x64-binaries.tar.gz
+	rm postgresql-10.14-1-linux-x64-binaries.tar.gz
+fi
+
+# start postgres
+# there may be already a postgres postgres running, anyway, stop it
+if [ -d "pgdata" ] ; then
+	${pgbin}/bin/pg_ctl -D pgdata  stop || true
+	rm -r pgdata
+fi
+${pgbin}/bin/initdb -D pgdata 
+${pgbin}/bin/pg_ctl -D pgdata -l pglog start
+
+# init postgres
+dropdb --if-exists contrib_regression
+createdb contrib_regression
+export PGPORT=${GPPORT}
